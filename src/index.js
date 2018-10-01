d6915c895b09beffc75a4b9c700863f4b65dacde
Nik <nik-pavlovich@mail.ru>
2018-10-01 22:06:20 +0300

    84p/14f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  
+
+  // first step 
+
+  if(array.length === 0){
+    console.log(typeof(array[0]));
+    return 0;
+  }
+
+  if(array.length === 1){
+    return 1;
+  }
+
+  // second step
+
+  var maxLength = 0;
+
+  //var noFamily = 0;
+
+
+  for(let i = 0; i < array.length; i++){
+
+    var root = array[i];
+
+    if(root !== undefined){
+
+      var counter = 1;
+      var left = array[i];
+      var right = array[i];
+      var l = 0;
+      var r = 0;
+      
+      do {
+
+        if(l>=0){
+          l = array.indexOf(left-1);
+
+          if(l>=0){
+            left = array[l];
+            delete array[l];
+            counter++;
+          }
+          
+        }
+        
+        if(r>=0){
+          r = array.indexOf(right+1);
+
+          if(r>=0){
+            right = array[r];
+            delete array[r];
+            counter++;
+          }
+        
+          
+        }
+
+
+
+
+      } while(r>=0 || l>=0);
+
+      if(counter > maxLength){
+        maxLength = counter;
+      }
+      
+    }
+  }
+
+
+  console.log('Max length - ' + maxLength);
+  return maxLength;
 }
